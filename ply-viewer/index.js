d5ea3c4ed935a77eb390ee797f247260e3abf049
vivek kumar <vivekxkr@gmail.com>
2025-03-12 01:04:29 +0530

    add patch filling in ply-viewer

--- ply-viewer/index.js
+++ ply-viewer/index.js
@@ -107,8 +107,9 @@ function render() {
     updatePoints();
 
     resetCanvas();
-    plotVertices();
-    plotFaces();
+    // plotVertices();
+    // plotFaces();
+    fillFaces();
 }
 
 function updatePoints() {
@@ -197,6 +198,16 @@ function plotFaces() {
     }
 }
 
+function fillFaces() {
+    for(let i = 0 ; i < faces.length ; i++) {
+        let pvs = [];
+        for(let j = 0 ; j < faces[i].length ; j++) {
+            pvs.push([Math.trunc(v2d[faces[i][j]][0]), Math.trunc(v2d[faces[i][j]][1])])
+        }
+        fillTriangle(pvs);
+    }
+}
+
 function plotLineXY(v1, v2) {
     bresenhamLine(Math.trunc(v1[0]), Math.trunc(v1[1]), Math.trunc(v2[0]), Math.trunc(v2[1]));
 }
@@ -222,40 +233,4 @@ function bresenhamLine(x1, y1, x2, y2) {
             y1 += sy;
         }
     }
-}
-
-function dotProduct(A, B) {
-    let rowsA = A.length, colsA = A[0].length,
-        rowsB = B.length, colsB = B[0].length;
-
-    if (colsA !== rowsB) {
-        throw new Error("Invalid dimensions for matrix multiplication");
-    }
-
-    let result = Array(rowsA).fill(0).map(() => Array(colsB).fill(0));
-
-    for (let i = 0; i < rowsA; i++) {
-        for (let j = 0; j < colsB; j++) {
-            for (let k = 0; k < colsA; k++) {
-                result[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-    return result;
-}
-
-function degToRad(degrees) {
-    return degrees * (Math.PI / 180);
-}
-
-// Function to convert Spherical Coordinates to 3D Cartesian Coordinates
-function sphericalToCartesian(thetaDeg, phiDeg, radius) {
-    const theta = degToRad(thetaDeg); // Convert to radians
-    const phi = degToRad(phiDeg);     // Convert to radians
-
-    const x = radius * Math.sin(phi) * Math.cos(theta);
-    const y = radius * Math.sin(phi) * Math.sin(theta);
-    const z = radius * Math.cos(phi);
-
-    return [ x, y, z ];
 }
